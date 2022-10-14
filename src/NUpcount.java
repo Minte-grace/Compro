6c6a2fd58b535753f138bbe95c8c75de7824d4c7
Minte-grace <mintesnottsegaye23@gmail.com>
2022-10-14 13:52:06 +0300

    n up count

--- src/NUpcount.java
+++ src/NUpcount.java
@@ -1,3 +1,25 @@
 public class NUpcount {
-    
+
+    public static void main(String[] args){
+        int[] testArray1 = {2,3,1,-6,8,-3,-1,2};
+        int[] testArray2 = {6,3,1};
+        int[] testArray3 = {1,2,-1,5,3,2,-3};
+        System.out.println(nUpCount(testArray1,5));
+        System.out.println(nUpCount(testArray2,6));
+        System.out.println(nUpCount(testArray3,20));
+    }
+   public static int nUpCount(int[] a, int n){
+        int count = 0;
+        int previousPartialSum = 0;
+        int partialSum = 0;
+        for(int i = 0; i < a.length; i++){
+           previousPartialSum = partialSum;
+           partialSum += a[i];
+           if(partialSum >= n && previousPartialSum < n){
+               count++;
+           }
+        }
+        return count;
+   }
+
 }
