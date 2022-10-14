f6e29a9ab7d14c2345d1676d53e1bb9690c54156
Minte-grace <mintesnottsegaye23@gmail.com>
2022-10-14 12:46:55 +0300

    fixes

--- src/NextPerfectSquare.java
+++ src/NextPerfectSquare.java
@@ -5,15 +5,20 @@ public class NextPerfectSquare{
         System.out.println(calculateNextPerfectSquare(0));
         System.out.println(calculateNextPerfectSquare(-5));
     }
-    static int calculateNextPerfectSquare(int n){
-        int nextPS = 0;
-        if(n < 0) {
-            return nextPS;
-        } else {
-            int sqrtOfNumber = (int) Math.sqrt(n) + 1;
-            nextPS = (int) Math.pow(sqrtOfNumber,2);
-            return nextPS;
+    public static int  calculateNextPerfectSquare(int n){
+        int nextPerfectSquare = 0;
+        if(n>=0){
+            int x = (int) Math.sqrt(n) + 1;
+            int y = (int) Math.pow(x,2);
+            nextPerfectSquare = y;
         }
-
+        return  nextPerfectSquare;
     }
 }
+
+
+
+
+
+
+
