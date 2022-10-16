9cb8df74fd44a0228b626f491d06ecd91c5be8c2
Minte-grace <mintesnottsegaye23@gmail.com>
2022-10-16 16:21:42 +0300

    common elemnts

--- src/CommonElements.java
+++ src/CommonElements.java
@@ -2,9 +2,9 @@ import java.util.Arrays;
 
 public class CommonElements {
     public static void main(String[] args){
-        int[] a = {1,2};
-        int[] b = {};
-        System.out.println(Arrays.toString(f(a,null)));
+        int[] a = {1,3,7,9};
+        int[] b = {7,1,9,3};
+        System.out.println(Arrays.toString(f(a,b)));
     }
     static int[] f(int[] first, int[] second){
         if(first == null || second == null){
@@ -12,23 +12,35 @@ public class CommonElements {
         }
         int count = 0;
         int[] common = new int[first.length];
-        for(int i=0; i< first.length; i++){
-            boolean com = false;
+        for(int i=0; i<first.length; i++){
+            boolean isCommon = false;
             for(int j=0; j< second.length; j++){
                 if(first[i]==second[j]){
-                    com = true;
+                    isCommon = true;
                     continue;
                 }
             }
-            if(com){
+            if(isCommon){
                 common[count] = first[i];
                 count++;
             }
         }
-        int[] commonValues = new int[count];
-        for(int x=0; x<commonValues.length; x++){
-            commonValues[x] = common[x];
+        int[] filteredCommon = new int[count];
+        for(int x=0; x<filteredCommon.length; x++){
+            filteredCommon[x]= common[x];
         }
-        return commonValues;
+        return filteredCommon;
     }
+
+
+
+
+
+
+
+
+
+
+
+
 }
