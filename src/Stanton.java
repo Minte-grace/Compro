528f83b0e401f13195bdc524f4e96d5132cc39a8
Minte-grace <mintesnottsegaye23@gmail.com>
2022-10-18 21:08:02 +0300

    stanton count

--- src/Stanton.java
+++ src/Stanton.java
@@ -4,23 +4,22 @@ public class Stanton {
         System.out.println(stantonMeasure(new int[]{0}));
         System.out.println(stantonMeasure(new int[]{3,1,1,4}));
         System.out.println(stantonMeasure(new int[]{1,3,1,1,3,3,2,3,3,3,4}));
-        System.out.println(stantonMeasure(new int[]{1,4,3,2,1,2,3,2}));
         System.out.println(stantonMeasure(new int[]{}));
+        System.out.println(stantonMeasure(new int[]{1,4,3,2,1,2,3,2}));
     }
     static int stantonMeasure(int[] a){
-        int count = 0;
-        int nextCount = 0;
-        for(int i=0; i< a.length; i++){
-            if(a[i] == 1) {
-                count++;
-            }
-        }
-        for(int i=0; i< a.length; i++){
-            if(count == a[i]) {
-                nextCount++;
-            }
-        }
-
-        return nextCount;
+       int stantonNumber=0;
+       int countSecond = 0;
+       for(int i=0; i<a.length; i++){
+           if(a[i]==1){
+               stantonNumber++;
+           }
+       }
+       for(int j=0; j<a.length; j++) {
+           if (a[j] == stantonNumber) {
+               countSecond++;
+           }
+       }
+       return countSecond;
     }
 }
