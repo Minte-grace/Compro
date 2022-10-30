af03dbd7fc3df52bf7cd01b7f2497b814667b77a
Minte-grace <mintesnottsegaye23@gmail.com>
2022-10-30 16:10:06 +0300

    length charachter

---
+++ src/LengthCharachter.java
@@ -0,0 +1,30 @@
+import java.util.Arrays;
+
+public class LengthCharachter {
+    public static void main(String[] args){
+        System.out.println(Arrays.toString(f(new char[]{'a','b','c'},0,4)));
+        System.out.println(Arrays.toString(f(new char[]{'a','b','c'},0,3)));
+        System.out.println(Arrays.toString(f(new char[]{'a','b','c'},0,2)));
+        System.out.println(Arrays.toString(f(new char[]{'a','b','c'},0,1)));
+        System.out.println(Arrays.toString(f(new char[]{'a','b','c'},1,3)));
+        System.out.println(Arrays.toString(f(new char[]{'a','b','c'},1,2)));
+        System.out.println(Arrays.toString(f(new char[]{'a','b','c'},1,1)));
+        System.out.println(Arrays.toString(f(new char[]{'a','b','c'},2,2)));
+        System.out.println(Arrays.toString(f(new char[]{'a','b','c'},2,1)));
+        System.out.println(Arrays.toString(f(new char[]{'a','b','c'},3,1)));
+        System.out.println(Arrays.toString(f(new char[]{'a','b','c'},1,0)));
+        System.out.println(Arrays.toString(f(new char[]{'a','b','c'},-1,2)));
+        System.out.println(Arrays.toString(f(new char[]{'a','b','c'},-1,-2)));
+        System.out.println(Arrays.toString(f(new char[]{},0,1)));
+    }
+    static char[] f(char[] a, int start, int len){
+        if(len > a.length || start+len > a.length || start<0 || len<0)  return null;
+        char[] returnedArray = new char[len];
+        int returnedArrayIndex = 0;
+            for(int i=start; i< start + len; i++){
+                returnedArray[returnedArrayIndex] = a[i];
+                returnedArrayIndex++;
+            }
+        return returnedArray;
+    }
+}
